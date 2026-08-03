9083ba767feb74d001eaeb7d7a487fa037aaf40c
kingsleysuoye556-arch <kingsleysuoye556@gmail.com>
2026-08-03 21:02:32 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,10 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! Bot is alive!")
 
-if __name__ == "__main__":
+def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
+
+if __name__ == "__main__":
+    main()

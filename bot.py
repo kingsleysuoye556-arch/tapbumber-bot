0566c2585cab9929654f1f716f2347fa840c52ad
kingsleysuoye556-arch <kingsleysuoye556@gmail.com>
2026-08-05 03:11:37 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,14 @@
-{
-  "message": "    app = ApplicationBuilder().token(TOKEN).build()",
-  "severity": "error",
-  "attributes": {
-    "level": "error"
-  },
-  "tags": {
-    "project": "f95df489-a4cc-43b1-8109-415d97443c1e",
-    "environment": "c03c9a67-d330-4e43-abfb-1dfd34780b07",
-    "service": "066deff1-3238-436d-a350-1ae5cb5f8750",
-    "deployment": "9ab9c11b-cfc7-4006-81d3-42a342b2a43c",
-    "replica": "ebff5dee-0ac8-40f3-93cc-a6ccd8da4a99"
-  },
-  "timestamp": "2026-08-03T23:14:37.898962139Z"
-}
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print("Someone sent /start")
+    await update.message.reply_text("Hello boss! Tapbumber bot is LIVE 🔥")
+
+print("Bot is starting...")
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.run_polling()

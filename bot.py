ef2b582f2598ebb69ae004cf2246eff2d4b950e5
kingsleysuoye556-arch <kingsleysuoye556@gmail.com>
2026-08-06 17:15:15 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 import os
 from telegram.ext import ApplicationBuilder, CommandHandler
 
-TOKEN ="8592807124:AAHCaDlwxYF5JW8aP4FoMn0AUEpPHLOgaTk"
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update, context):
-    await update.message.reply_text("TapBumber is ONLINE! ✅\nSend /tap to test")
+    await update.message.reply_text("TapBumber is ONLINE! ✅")
 
 async def tap(update, context):
     await update.message.reply_text("Tapped! +1 💰")
@@ -12,4 +12,5 @@ async def tap(update, context):
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("tap", tap))
+print("Bot is running...")
 app.run_polling()
